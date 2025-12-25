ac6e5c29f74c36588aa3a4e928c9ddafd7299826
ekasandyualia-gtm <ekasandyaulia@gmail.com>
2025-12-26 06:04:18 +0700

    set again model

--- .history/MLProject/Modelling_tunning_20251226055256.py
+++ .history/MLProject/Modelling_tunning_20251226055256.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mlflow
+import mlflow.sklearn
 import dagshub
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
@@ -19,21 +20,20 @@ dagshub.init(
 
 mlflow.set_experiment("Titanic-Advanced-Tuning")
 
-# no auto log
-mlflow.autolog(disable=True)
+# auto log
+mlflow.autolog(log_models=True)
 
 # Load data
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 X_train = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "X_train.csv"))
 X_test  = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "X_test.csv"))
-y_train = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "y_train.csv")).values.ravel()
-y_test  = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "y_test.csv")).values.ravel()
+y_train = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "y_train.csv"))
+y_test  = pd.read_csv(os.path.join(BASE_DIR, "processed_data", "y_test.csv"))
 
 X_train = X_train.select_dtypes(include=["int64", "float64"])
-X_test  = X_test.select_dtypes(include=["int64", "float64"])
+X_test = X_test.select_dtypes(include=["int64", "float64"])
 
-# Hyperparameter loop
 n_estimators_list = [50, 100]
 max_depth_list = [5, 10]
 
@@ -41,11 +41,6 @@ for n_estimators in n_estimators_list:
     for max_depth in max_depth_list:
 
         with mlflow.start_run():
-
-            # log parameters
-            mlflow.log_param("n_estimators", n_estimators)
-            mlflow.log_param("max_depth", max_depth)
-            mlflow.log_param("model", "RandomForestClassifier")
 
             model = RandomForestClassifier(
                 n_estimators=n_estimators,
@@ -58,10 +53,7 @@ for n_estimators in n_estimators_list:
             y_pred = model.predict(X_test)
             acc = accuracy_score(y_test, y_pred)
 
-            # log metric
-            mlflow.log_metric("accuracy", acc)
-
-            # Artifact 1: Confusion Matrix
+            # Confusion Matrix
             cm = confusion_matrix(y_test, y_pred)
             plt.figure()
             sns.heatmap(cm, annot=True, fmt="d")
@@ -71,10 +63,9 @@ for n_estimators in n_estimators_list:
             cm_path = f"artifacts/confusion_matrix_{n_estimators}_{max_depth}.png"
             plt.savefig(cm_path)
             plt.close()
-
             mlflow.log_artifact(cm_path)
 
-            # Artifact 2: Feature Importance
+            # Feature Importance
             fi = pd.DataFrame({
                 "feature": X_train.columns,
                 "importance": model.feature_importances_
@@ -82,7 +73,6 @@ for n_estimators in n_estimators_list:
 
             fi_path = f"artifacts/feature_importance_{n_estimators}_{max_depth}.csv"
             fi.to_csv(fi_path, index=False)
-
             mlflow.log_artifact(fi_path)
 
             print(f"Run selesai | accuracy={acc}")
