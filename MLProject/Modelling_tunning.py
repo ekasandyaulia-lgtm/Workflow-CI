cd2e9c4ac2955e615c5b268ff750fbca4cee7de1
Aulia <ekasandyaulia@gmail.com>
2025-12-28 15:35:23 +0700

    Update Modelling_tunning.py

--- MLProject/Modelling_tunning.py
+++ MLProject/Modelling_tunning.py
@@ -8,6 +8,13 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
+mlflow.autolog(
+    log_input_examples=True,
+    log_model_signatures=True,
+    log_models=True,  # IMPORTANT: untuk log model otomatis
+    log_datasets=True
+)
+
 assert os.getenv("DAGSHUB_TOKEN") is not None, "DAGSHUB_TOKEN is missing"
 
 dagshub.init(
@@ -22,6 +29,7 @@ mlflow.set_tracking_uri(
 
 mlflow.set_experiment("Titanic-Advanced-Tuning")
 
+# Load data
 X_train = pd.read_csv("processed_data/X_train.csv")
 X_test  = pd.read_csv("processed_data/X_test.csv")
 y_train = pd.read_csv("processed_data/y_train.csv").values.ravel()
@@ -31,45 +39,42 @@ X_train = X_train.select_dtypes(include=["int64", "float64"])
 X_test  = X_test.select_dtypes(include=["int64", "float64"])
 
 with mlflow.start_run():
-
+    # Parameters
     n_estimators = 100
     random_state = 42
-
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("random_state", random_state)
-
+    
+    # Model
     model = RandomForestClassifier(
         n_estimators=n_estimators,
         random_state=random_state
     )
-
+    
+    # Training 
     model.fit(X_train, y_train)
-
+    
+    # Evaluation
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-    mlflow.log_metric("accuracy", acc)
-
-    # Log model (TANPA registry)
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model"
-    )
-
+    
+    # Manual logging tambahan (untuk kriteria advance)
+    mlflow.log_metric("test_accuracy", acc)
+    
     # Artifact 1: Confusion Matrix
     cm = confusion_matrix(y_test, y_pred)
     disp = ConfusionMatrixDisplay(cm)
     disp.plot()
+    plt.title(f"Confusion Matrix (Accuracy: {acc:.4f})")
     plt.savefig("confusion_matrix.png")
     plt.close()
     mlflow.log_artifact("confusion_matrix.png")
-
+    
     # Artifact 2: Feature Importance
     fi = pd.DataFrame({
         "feature": X_train.columns,
         "importance": model.feature_importances_
     }).sort_values(by="importance", ascending=False)
-
+    
     fi.to_csv("feature_importance.csv", index=False)
     mlflow.log_artifact("feature_importance.csv")
 
-print("MLflow Advanced Training Completed"
+print("MLflow Advanced Training Completed")
