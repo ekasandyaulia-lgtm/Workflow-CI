2492e10a220e75c3235d8e7e57c61b481ad66799
Aulia <ekasandyaulia@gmail.com>
2025-12-29 09:30:18 +0700

    Update Modelling_tunning.py

--- MLProject/Modelling_tunning.py
+++ MLProject/Modelling_tunning.py
@@ -5,16 +5,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 import mlflow
 import mlflow.sklearn
-import dagshub
 
 DAGSHUB_TOKEN = os.environ["DAGSHUB_TOKEN"]
 
-# Inisialisasi DagsHub dan MLflow tracking
-
+# Set tracking URI dan experiment
 mlflow.set_tracking_uri(
-    f"https://{os.getenv('DAGSHUB_TOKEN')}@dagshub.com/ekasandyaulia-lgtm/SMLS_Eka_Sandy_Aulia_Puspitasari.mlflow"
+    f"https://{DAGSHUB_TOKEN}@dagshub.com/ekasandyaulia-lgtm/SMLS_Eka_Sandy_Aulia_Puspitasari.mlflow"
 )
-
 mlflow.set_experiment("Titanic-Advanced-Tuning")
 
 # Load dataset
@@ -23,32 +20,28 @@ X_test  = pd.read_csv("processed_data/X_test.csv")
 y_train = pd.read_csv("processed_data/y_train.csv").values.ravel()
 y_test  = pd.read_csv("processed_data/y_test.csv").values.ravel()
 
+# Pilih kolom numerik
 X_train = X_train.select_dtypes(include=["int64", "float64"])
 X_test  = X_test.select_dtypes(include=["int64", "float64"])
 
-# Mulai run manual
 with mlflow.start_run() as run:
     RUN_ID = run.info.run_id
     print(f"MLflow Run ID: {RUN_ID}")
 
-    # Model dan parameter
-    params = {
-        "C": 1.0,
-        "max_iter": 200,
-        "solver": "liblinear"
-    }
+    # Model
+    params = {"C": 1.0, "max_iter": 200, "solver": "liblinear"}
     model = LogisticRegression(**params)
     model.fit(X_train, y_train)
 
-    # Prediksi dan evaluasi
+    # Evaluasi
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
-    # Manual logging
+    # Log parameter dan metric
     mlflow.log_params(params)
     mlflow.log_metric("accuracy", acc)
 
-    # Artefak 1: Confusion Matrix
+    # Log artefak: Confusion Matrix
     cm = confusion_matrix(y_test, y_pred)
     disp = ConfusionMatrixDisplay(cm)
     disp.plot()
@@ -57,7 +50,7 @@ with mlflow.start_run() as run:
     plt.close()
     mlflow.log_artifact("confusion_matrix.png")
 
-    # Artefak 2: Koefisien model
+    # Log artefak: Koefisien model
     coef_df = pd.DataFrame(model.coef_, columns=X_train.columns)
     coef_df.to_csv("model_coefficients.csv", index=False)
     mlflow.log_artifact("model_coefficients.csv")
